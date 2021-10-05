8ad929bed0d959792e96ad8ffe1fff4133a74954
ilya rudenko <webilya29@gmail.com>
2021-10-05 21:32:57 +0300

    first commit

---
+++ Filter/Filter/FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterApp.swift
+//  Filter
+//
+//  Created by Ilya on 05.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
